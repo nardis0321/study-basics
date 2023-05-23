162f4facbad55df2b6cec328945fa543f8e4c027
luke <luke@218.38.137.28>
2023-05-23 23:37:22 +0900

    refactor: modify code to test codes using Baekjoon system

--- src/algorithm/greedy/Change.java
+++ src/algorithm/greedy/Change.java
@@ -1,35 +1,29 @@
 package algorithm.greedy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Change {
 	/**
-	 * 최소 개수의 거스름돈 주 
-	 * @param amount 거슬러 주어야 하는 액수
-	 * @return changes 최소 거스름돈 개수
+	 * 백준 5585번 거스름돈
 	 */
-	public int minChanges(int amount) {
-		int changes = 0;
-		
-		int[] coins = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
-		for(int i = 0; i<=coins.length-1; i++) {
-			if(amount<10) {
-				break;
-			}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int amount = Integer.parseInt(br.readLine());
+
+        amount = 1000 - amount;
+        int changes = 0;	
+		int[] coins = {500, 100, 50, 10, 5, 1};
+
+        for(int i = 0; i<=coins.length-1; i++) {
 			int cnt = amount/coins[i];
-			changes += cnt;
-			amount -= cnt*coins[i];
-		}
-		return changes;
-	}
-
-	public static void main(String[] args) {
-		Change c1 = new Change();
-		int change1 = c1.minChanges(380);
-		System.out.println(change1);
-		
-		Change c2 = new Change();
-		int change2 = c2.minChanges(54520);
-		System.out.println(change2);
-
+			if(cnt>=1) {
+				changes += cnt;
+				amount -= cnt*coins[i];
+			}
+        }
+        System.out.println(changes);
 	}
 }
 
