7f39031905a1bacc2135c9c72b00e34be6f37dca
luke <luke@218.38.137.28>
2023-05-30 01:23:23 +0900

    refactor: concern scalability, check memory usage and running time

--- src/basic100/NabooFleetData.java
+++ src/basic100/NabooFleetData.java
@@ -1,15 +1,35 @@
 package basic100;
 
 public class NabooFleetData {
+	int[] comlumnSpace = {15, 15, 11, 10};
+
+	String[][] data = 
+		{{"SHIP NAME",      "CLASS",          "DEPLOYMENT", "IN SERVICE"},
+				{"N2 Bombrt",      "Heavy Fighter",  "Limited",    "21"},
+				{"J-Type 327",     "Light Combat",   "Unlimited",  "1"},
+				{"NX Cruiser",     "Medium Fighter", "Limited",    "18"},
+				{"N1 Starfighter", "Medium Fighter", "Unlimited",  "25"},
+				{"Royal Cruiser",  "Light Combat",   "Limited",    "4"}};
 
 	public static void main(String[] args) {
-		System.out.println(
-				"SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n" + 
-				"N2 Bomber      Heavy Fighter  Limited    21        \n" + 
-				"J-Type 327     Light Combat   Unlimited  1         \n" + 
-				"NX Cruiser     Medium Fighter Limited    18        \n" + 
-				"N1 Starfighter Medium Fighter Unlimited  25        \n" + 
-				"Royal Cruiser  Light Combat   Limited    4         ");
-	}
+		long beforeTime = System.currentTimeMillis();
 
+		NabooFleetData data = new NabooFleetData();
+		for(int i=0; i<=data.data.length-1; i++) {
+			for(int j=0; j<=data.data[i].length-1; j++) {
+				System.out.print(data.data[i][j]);
+				int blankQuantity = data.comlumnSpace[j] - data.data[i][j].length();
+				for(int k=0; k<=blankQuantity-1; k++) {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+		long afterTime = System.currentTimeMillis();
+		long duration = (afterTime - beforeTime);
+		System.out.println("걸리는 시간: "+duration+"ms");
+		System.out.println("사용 메모리: "+Runtime.getRuntime().totalMemory()/(1024*1024)+"MB");
+
+	}
 }
