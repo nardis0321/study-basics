e949e52c94ac02430ae31c14f3a28b6104ea68ce
nardis0321 <nardis0321@gmail.com>
2023-06-03 00:29:09 +0900

    refactor: concern scalability

--- src/basic100/WhatTheYodaSaid.java
+++ src/basic100/WhatTheYodaSaid.java
@@ -5,18 +5,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class WhatTheYodaSaid {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int sentenceQuantity = Integer.parseInt(br.readLine());
+	private static int NUM_OF_WORDS_TO_MOVE = 2; //뒤로 옮길 단어의 수
+	
+	public StringBuilder makeItYodaWay(BufferedReader br) throws NumberFormatException, IOException {
+		int sentenceQuantity = Integer.parseInt(br.readLine()); //바꿀 문장 수 입력받기
+		StringBuilder whatYodaSay = new StringBuilder();
 		
 		while(sentenceQuantity-->0) {
-			String[] words = br.readLine().split(" ");
-			for(int i=2; i<=words.length-1; i++) {
-				System.out.print(words[i]+" ");
+			String[] words = br.readLine().split(" "); //문장 입력받아 단어로 쪼갬
+			br.close();
+			
+			for(int i = NUM_OF_WORDS_TO_MOVE; i<=words.length-1; i++) {
+				whatYodaSay.append(words[i]).append(" ");
 			}
-			System.out.println(words[0]+" "+words[1]);
+			for(int i = 0; i<=NUM_OF_WORDS_TO_MOVE-2; i++) {
+				whatYodaSay.append(words[i]).append(" ");
+			}
+			whatYodaSay.append(words[NUM_OF_WORDS_TO_MOVE-1]).append("\n");
 		}
+
+		return whatYodaSay;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		WhatTheYodaSaid yoda = new WhatTheYodaSaid();
+		System.out.println(yoda.makeItYodaWay(br));
+		
 	}
 
 }
