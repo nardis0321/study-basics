7ae933328985386c4d0ef74a3dc05fdd9e3f2b39
nardis0321 <nardis0321@gmail.com>
2023-07-12 03:18:58 +0900

    refactor: find 2 instead of 7 using for statement

--- src/algorithm/bruteforce/SevenDwarfs.java
+++ src/algorithm/bruteforce/SevenDwarfs.java
@@ -2,9 +2,7 @@ package algorithm.bruteforce;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class SevenDwarfs {
 	/**
@@ -19,34 +17,44 @@ public class SevenDwarfs {
 
 		int[] heights = new int[9];		
 		for(int i=0; i<candidates; i++) {
-			System.out.println(i);
 			heights[i] = sc.nextInt();
 		}
+		sc.close();
 		Arrays.sort(heights); //오름차순으로 출력해야 하기 때문에 미리 정렬
 
 		
 		// 진짜 난쟁이를 찾아라 7명 키의 합이 100
 		int result = 0;
-		Set<Integer> pick = new HashSet<>(); 
-		while(! (result == 100)) {
-			// 합이 100이 될 때까지 반복
-			result = 0;
-			pick.clear();
 
-			int pickNum = 7;
-			while(pick.size()<pickNum) {
-				// 더할 7개의 숫자를 고르기
-				pick.add((int) ((Math.random()*10+1)%9));	
-			}
-
-			for(int picked : pick) {
-				result += heights[picked];
+		//1. 키를 전부 더하기
+		for(int height : heights) {
+			result += height;
+		}
+		//2. 뺄 2개의 인덱스를 찾기
+		int firstFakeIndex = 0;
+		int secondFakeIndex = 0;
+		for(int i=0; i<candidates; i++) { //조합을 탐색 1~9 + 2~9
+			for(int j=1; j<candidates; j++) {
+				
+				if(i==j) { //존재하지 않는 경우 제외
+					continue;
+				}
+				
+				int temp = result - heights[i] - heights[j];
+				if(temp == 100) {
+					firstFakeIndex = i;
+					secondFakeIndex = j;
+					break;
+				}
 			}
 		}
+		
 
 		// 진짜 난쟁이의 키를 출력
-		for(int picked : pick) {
-			System.out.println(heights[picked]);
+		for(int i=0; i<candidates; i++) {
+			if(i != firstFakeIndex && i != secondFakeIndex) {
+				System.out.println(heights[i]);
+			}
 		}
 
 	}
