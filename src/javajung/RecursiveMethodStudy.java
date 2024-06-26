708a138201240f719cc037c71c24d6bc1171fa9f
nardis0321 <nardis0321@gmail.com>
2024-06-26 17:50:05 +0900

    study: study recursive method making another one

--- src/javajung/RecursiveMethodStudy.java
+++ src/javajung/RecursiveMethodStudy.java
@@ -16,10 +16,30 @@ public class RecursiveMethodStudy {
         factorial(n);
     }
 
+    // 피보나치 수열 구현
+    static int x = 1;
+    static int y = 1;
+    public static void fibonacci(int n){
+        if (result == n){
+            return;
+        }
+
+        result = x + y;
+
+        x = y;
+        y = result;
+        System.out.print(result+" "); // 수열 출력
+        fibonacci(n);
+    }
+
     public static void main(String[] args) {
         // 팩토리얼 테스트
-        System.out.println("factorial: ");
+        System.out.print("factorial: ");
         factorial(5);
         System.out.println(result);
+
+        // 피보나치 테스트
+        System.out.print("fibonacci: 1 1 ");
+        fibonacci(55);
     }
 }
