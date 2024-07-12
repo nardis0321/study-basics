08b5170283e36764c40d0e76c17024f6064d7f64
nardis0321 <nardis0321@gmail.com>
2024-07-12 17:45:48 +0900

    study: make sum method using recursion

--- src/javajung/RecursiveMethodStudy.java
+++ src/javajung/RecursiveMethodStudy.java
@@ -22,6 +22,20 @@ public class RecursiveMethodStudy {
         return fibonacci(n-1)+fibonacci(n-2);
     }
 
+     /*
+     100(num)부터 1까지 더하기
+        문제를 정의하기 : return 100 + ... + 1 (100부터 1까지 모두 더한 결과)
+        베이스 조건 : 1에서 멈추기
+        1 => return 1
+        2 => return 2+1
+     */
+    public static int sumNumbers(int num){
+        if(num == 1)
+            return 1;
+        return num + sumNumbers(num-1);
+        //
+    }
+
     public static void main(String[] args) {
         // 팩토리얼 테스트
         System.out.print("factorial: ");
@@ -32,6 +46,12 @@ public class RecursiveMethodStudy {
         System.out.print("fibonacci: ");
         result = fibonacci(5);
         System.out.println("fibonacci(5) = " + result);
+
+        // 더하기 테스트
+        System.out.println("sum test: ");
+        System.out.println("sumNumbers(5) = " + sumNumbers(5));
+        System.out.println("sumNumbers(10) = " + sumNumbers(10));
+        System.out.println("sumNumbers(100) = " + sumNumbers(100));
 
     }
 }
